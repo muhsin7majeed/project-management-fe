09c8b6211ae305101a8b84031bd13138dec49fe4
muhsin <muhsin@springworks.in>
2022-07-14 23:47:37 +0530

    feature: Fetch Projects and Handle Errors

--- src/pages/project/projects/index.jsx
+++ src/pages/project/projects/index.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@apollo/client";
 import {
   Box,
   Button,
@@ -8,28 +9,18 @@ import {
   StatNumber,
   useColorModeValue,
   Heading,
+  Center,
 } from "@chakra-ui/react";
+import DefaultSpinner from "components/loaders/DefaultSpinner";
+import SomethingWentWrong from "components/SomethingWentWrong";
 import { Link } from "react-router-dom";
-
-const PROJECTS = [
-  {
-    id: 1,
-    name: "Project One",
-    status: "Completed",
-  },
-  {
-    id: 2,
-    name: "Project Two",
-    status: "In Progress",
-  },
-  {
-    id: 3,
-    name: "Project Three",
-    status: "Started",
-  },
-];
+import { GET_PROJECTS } from "../queries";
 
 function Projects() {
+  const { data, loading, error, refetch, networkStatus } = useQuery(GET_PROJECTS);
+
+  console.log({ data, loading, networkStatus });
+
   return (
     <>
       <Box pt={5}>
@@ -37,35 +28,44 @@ function Projects() {
           Projects
         </Heading>
 
-        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
-          {PROJECTS.map((project) => (
-            <Stat
-              key={project.id}
-              px={{ base: 2, md: 4 }}
-              py={"5"}
-              shadow={"xl"}
-              border={"1px solid"}
-              borderColor={useColorModeValue("gray.800", "gray.500")}
-              rounded={"lg"}
-            >
-              <Flex justifyContent={"space-between"}>
-                <Box pl={{ base: 2, md: 4 }}>
-                  <StatLabel fontWeight={"medium"}>{project.status}</StatLabel>
+        {loading && <DefaultSpinner />}
+        {/* <SomethingWentWrong onClick={handleRefetch} /> */}
 
-                  <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
-                    {project.name}
-                  </StatNumber>
-                </Box>
+        {error && <SomethingWentWrong onClick={refetch} />}
 
-                <Box my={"auto"} color={useColorModeValue("gray.800", "gray.200")} alignContent={"center"}>
-                  <Link to={`/project/${project.id}`}>
-                    <Button>View</Button>
-                  </Link>
-                </Box>
-              </Flex>
-            </Stat>
-          ))}
-        </SimpleGrid>
+        {data?.projects?.length === 0 && <Center>No Projects Found</Center>}
+
+        {data?.projects?.length && (
+          <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
+            {data.projects.map((project) => (
+              <Stat
+                key={project.id}
+                px={{ base: 2, md: 4 }}
+                py={"5"}
+                shadow={"xl"}
+                border={"1px solid"}
+                borderColor={useColorModeValue("gray.800", "gray.500")}
+                rounded={"lg"}
+              >
+                <Flex justifyContent={"space-between"}>
+                  <Box pl={{ base: 2, md: 4 }}>
+                    <StatLabel fontWeight={"medium"}>{project.status}</StatLabel>
+
+                    <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
+                      {project.name}
+                    </StatNumber>
+                  </Box>
+
+                  <Box my={"auto"} color={useColorModeValue("gray.800", "gray.200")} alignContent={"center"}>
+                    <Link to={`/project/${project.id}`}>
+                      <Button>View</Button>
+                    </Link>
+                  </Box>
+                </Flex>
+              </Stat>
+            ))}
+          </SimpleGrid>
+        )}
       </Box>
     </>
   );
