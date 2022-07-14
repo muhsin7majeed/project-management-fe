20932406ebe57599ab242bc44259e7c93e280687
muhsin <muhsin@springworks.in>
2022-07-14 23:47:08 +0530

    feature: Create Loader, SomethingWentWrong Component

---
+++ src/components/loaders/DefaultSpinner.jsx
@@ -0,0 +1,11 @@
+import { Center, Spinner } from "@chakra-ui/react";
+
+function DefaultSpinner() {
+  return (
+    <Center>
+      <Spinner size="md" />
+    </Center>
+  );
+}
+
+export default DefaultSpinner;
