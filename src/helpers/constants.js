3e92fde9a33fd25e1e6a026240ce91e6fa201469
muhsin <muhsin7ak@gmail.com>
2022-08-28 16:34:05 +0530

    CHANGE: Break down constants file.

--- src/helpers/constants.js
+++
@@ -1,8 +0,0 @@
-export const CONSTANT_URI = "http://localhost:8080/graphql";
-
-export const PROJECT_STATUS = [
-  { label: "Pending", value: "Pending" },
-  { label: "In Progress", value: "In Progress" },
-  { label: "Paused", value: "Paused" },
-  { label: "Completed", value: "Completed" },
-];
