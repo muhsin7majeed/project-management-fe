1935d47d603786bc799d946716a386f4f1f8d9ca
muhsin <muhsin7ak@gmail.com>
2022-08-28 16:38:21 +0530

    CHANGE: Move SelectStatus & SelectClient to ProjectForm

--- src/pages/project/projects/projectForm/SelectStatus.jsx
+++ src/pages/project/projects/projectForm/SelectStatus.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import { Select } from "@chakra-ui/react";
 
-import { PROJECT_STATUS } from "helpers/constants";
+import { PROJECT_STATUS_LIST } from "helpers/constants/project";
 
 function SelectStatus({ value, onChange }) {
   return (
     <Select placeholder="Select status" onChange={onChange} value={value}>
-      {PROJECT_STATUS.map((status) => (
+      {PROJECT_STATUS_LIST.map((status) => (
         <option key={status.value} value={status.value}>
           {status.label}
         </option>
